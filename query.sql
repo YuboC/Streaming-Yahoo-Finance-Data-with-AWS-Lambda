58af067175d89bbc0cc23e53d6c823df54cd3238
YuboC <37256200+YuboC@users.noreply.github.com>
2020-05-25 20:00:56 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-SELECT name, SUBSTRING(ts, 12, 2) AS hour , MAX(high) AS max_high 
-FROM "2020" 
-GROUP BY  SUBSTRING(ts, 12, 2), name 
-ORDER BY name, SUBSTRING(ts, 12, 2)
+SELECT * FROM (SELECT a.name, a.hour, a.ts, b.max_high FROM (SELECT name, high, ts, SUBSTRING(ts, 12, 2) AS hour  FROM "2020" db) a
+INNER JOIN (SELECT name, SUBSTRING(ts, 12, 2) AS hour, MAX(high) AS max_high FROM "2020" GROUP BY name, SUBSTRING(ts, 12, 2)) b
+ON a.name = b.name AND a.hour = b.hour AND a.high = b.max_high)
+ORDER BY name, hour
